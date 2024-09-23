c6fd143c5cb33c3689b59486b579be8e691670df
Fiyinfolu <lolafiyin@gmail.com>
2024-09-24 00:46:51 +0100

    add a script that define a function to perform basic arithmetic operation

---
+++ fns_and_dsa/arithmetic_operations.py
@@ -0,0 +1,25 @@
+##Author: Fiyin
+## this script, define a function that performs
+## basic arithmetic operations. This function, 
+## perform_operation such as addition, subtraction
+## multiplication and division
+
+
+def perform_operation(num1, num2, operation):
+    result = 0
+    match operation:
+        case "add":
+            result = num1 + num2
+            print(f"Result: {result}")
+        case "subtract":
+            result = num1 - num2
+            print(f"Result: {result}")
+        case "multiply":
+            result = num1 * num2
+            print(f"Result: {result}")
+        case "divide":
+            if num2 == 0:
+                print("Cannot divide by zero")
+            else:
+                result = num1 / num2
+                print(f"Result: {result}")
