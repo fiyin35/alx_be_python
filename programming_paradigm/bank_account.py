7679fef7c3ae523ebe3922329592655d95631864
Fiyinfolu <lolafiyin@gmail.com>
2024-10-04 01:01:38 +0100

    modify the display method

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -20,5 +20,5 @@ class BankAccount:
             return False
 
     def display_balance(self):
-        print(f"Current balance: {self.account_balance}")
+        print(f"Current Balance: {self.account_balance}")
 
