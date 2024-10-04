455ff383a0aa58d2cf219c4a17853dfb9bdb5e1d
Fiyinfolu <lolafiyin@gmail.com>
2024-10-04 01:10:29 +0100

    modification base checker recognition

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -7,18 +7,17 @@ class BankAccount:
 
     def deposit(self, amount):
         self.account_balance += amount
-        print(f"{amount} deposited")
+        print(f"Deposited ${amount}")
         return self.account_balance
 
     def withdraw(self, amount):
         if self.account_balance >= amount:
             self.account_balance -= amount
-            print(f"{amount} withdrawn")
+            print(f"Withdrew: ${amount}")
             return self.account_balance
         else:
-            print("Insufficient funds")
             return False
 
     def display_balance(self):
-        print(f"Current Balance: {self.account_balance}")
+        print(f"Current Balance: ${self.account_balance}")
 
