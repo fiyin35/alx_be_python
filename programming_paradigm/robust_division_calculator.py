42e3df5443136d43e34cc38564222f7f7494a4c3
Fiyinfolu <lolafiyin@gmail.com>
2024-10-04 02:19:40 +0100

    modify division calculator script

--- programming_paradigm/robust_division_calculator.py
+++ programming_paradigm/robust_division_calculator.py
@@ -8,7 +8,10 @@ def safe_divide(numerator, denominator):
         print(f"The result of the division is {divide}")
     except ZeroDivisionError:
         print("Error: Cannot divide by zero.")
+        return ""
     except ValueError:
         print("Error: Please enter numeric values only.")
+        return ""
     except Exception as e:
         print(e)
+        return ""
