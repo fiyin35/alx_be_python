2d16943d9b21c871367bfd3741cf4879f3c7528e
Fiyinfolu <lolafiyin@gmail.com>
2024-10-04 01:33:32 +0100

    modification base on checker recommendation

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -17,5 +17,5 @@ class BankAccount:
             return False
 
     def display_balance(self):
-        print(f"Current Balance: ${float(self.account_balance)}")
+        print(f"Current Balance: ${self.account_balance:.2f}")
 
