2eba6acc1659e480ef911ef8bcdaed71a6934c41
Fiyinfolu <lolafiyin@gmail.com>
2024-10-04 01:27:41 +0100

    modification base on checker recommendation

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -2,20 +2,20 @@
 
 class BankAccount:
 
-    def __init__(self, account_balance: float):
+    def __init__(self, account_balance = 0):
         self.account_balance = account_balance
 
     def deposit(self, amount: float):
         self.account_balance += amount
-        return self.account_balance
+        return float(self.account_balance)
 
     def withdraw(self, amount: float):
         if self.account_balance >= amount:
             self.account_balance -= amount
-            return self.account_balance
+            return float(self.account_balance)
         else:
             return False
 
     def display_balance(self):
-        print(f"Current Balance: ${self.account_balance}")
+        print(f"Current Balance: ${float(self.account_balance)}")
 
