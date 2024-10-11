e40baf49cdabf11cdc529be833af563d216eb432
Fiyinfolu <lolafiyin@gmail.com>
2024-10-11 20:44:30 +0100

    fixed the issue based on checkers validation

--- oop/library_system.py
+++ oop/library_system.py
@@ -24,7 +24,7 @@ class PrintBook(Book):
         self.page_count = page_count
 
     def __str__(self):
-        return f"PrintBook: self.title} by {self.author}, Page Count: {self.page_count}"
+        return f"PrintBook: {self.title} by {self.author}, Page Count: {self.page_count}"
 
 class Library:
 
